52d8a4f7a3dacdcf8b286962a9fe59e28862955b
rishabhdhara <rishabh.dhara01@gmail.com>
2026-08-05 18:35:35 +0530

    first java prog of line printing

---
+++ src/main.java
@@ -0,0 +1,23 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        //THIS MY FIRST JAVA PROGRAM
+/*
+THIS
+IS
+A
+MULTILINE
+CODE
+JAVA
+ */
+        System.out.println("hello");;
+        System.out.println("  wassuppppp ! ");
+        System.out.println("NIGAAAAAAAAAA");
+        System.out.println("jaana");
+
+    }
+
+}
