05056ec1d24415a7d5a4290824f02214d9d6f4f6
rishabhdhara <rishabh.dhara01@gmail.com>
2026-08-05 19:30:41 +0530

    learnt beginner variable: primitive

--- src/main.java
+++ src/main.java
@@ -18,6 +18,48 @@ JAVA
         System.out.println("NIGAAAAAAAAAA");
         System.out.println("jaana");
 
+        int age = 45;
+        int year = 2026 ;
+        int quantity = 55 ;
+        System.out.println("The year is " + year);
+        System.out.println("I posses " + quantity + " coke");
+        System.out.println("My age is " + age);
+
+        double price = 29.99;
+        double gpa = 5.5;
+        double temperature = 32.4;
+
+        System.out.println("This is the worst day i have ever seen");
+        System.out.println("I got " + gpa +" cgpa");
+        System.out.println("The gas price is a whopping " + price + " dollars");
+        System.out.println("and the temperature is fucking " +temperature + " celcius");
+
+        char grade = 'b' ;
+        char symbol = '@' ;
+        char currency = '¥';
+
+        System.out.println("I got a " + grade + " grade");
+        System.out.println(symbol);
+        System.out.println(currency);
+
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        System.out.println("I am a studen and it is " + isStudent );
+        System.out.println(forSale );
+        System.out.println(isOnline);
+
+        if(isStudent == true) {
+            System.out.println("you are a student !");
+        }
+        else{
+
+            System.out.println("STEP OF BITCH! ");
+
+
+        }
     }
 
 }
