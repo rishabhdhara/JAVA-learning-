ad5cb6fdb328c559b49f54212890f118221977a4
rishabhdhara <rishabh.dhara01@gmail.com>
2026-08-12 00:49:09 +0530

    Learned agumented assignment operations

--- src/main.java
+++ src/main.java
@@ -4,17 +4,38 @@ public class main {
 
         //Arithmetic Operations
 
-        int x = 100;
-        int y = 20;
-     int z;
+//        int x = 100;
+//        int y = 20;
+//     int z;
+//
+//        //z = x + y;
+//        //z = x- y;
+//        //z = x * y;
+//        //z = x / y;
+//        //z = x % y; (% used for reemainder and is called modulas here)
+//
+//        System.out.println(z);
 
-        //z = x + y;
-        //z = x- y;
-        //z = x * y;
-        //z = x / y;
-        //z = x % y; (% used for reemainder and is called modulas here)
+        /* Agumented assignment operators/operations*/
+        // insted of z here we are gomma contain result in x itself like pehle x = x + y ; gives a result 120 but using
+        // AAO we can write x += y; and it gives the same answers and it is shortcut too!!
 
-        System.out.println(z);
+//        int x = 100;
+//        int y = 20;
+
+//        x += y;
+//        x -= y;
+//        x *= y;
+//        x /= y;
+//        x %= y;
+
+        double x = 10.99120;
+        double y = 833.987;
+
+//        x *= y;
+        x /= y;
+
+        System.out.println(x);
 
 
 
