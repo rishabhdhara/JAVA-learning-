0159b55c6bc420bf52d1930067b4565afd7f9921
rishabhdhara <rishabh.dhara01@gmail.com>
2026-08-12 00:40:35 +0530

    Learned basic arithmetic ops

---
+++ src/main.java
@@ -0,0 +1,26 @@
+public class main {
+
+    public static void main(String[] args){
+
+        //Arithmetic Operations
+
+        int x = 100;
+        int y = 20;
+     int z;
+
+        //z = x + y;
+        //z = x- y;
+        //z = x * y;
+        //z = x / y;
+        //z = x % y; (% used for reemainder and is called modulas here)
+
+        System.out.println(z);
+
+
+
+
+
+
+
+    }
+}
