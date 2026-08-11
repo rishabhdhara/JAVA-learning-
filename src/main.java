478d5d8529cb86123ad2d102ad6679c02432e05d
rishabhdhara <rishabh.dhara01@gmail.com>
2026-08-11 21:50:54 +0530

    reinstalled git /accedentally deleted it

--- src/main.java
+++ src/main.java
@@ -7,83 +7,87 @@ public class main {
 
     public static void main(String[] args) {
 
-        //THIS MY FIRST java PROGRAM
-
-
-/*
-THIS
-IS
-A
-MULTILINE
-CODE
-JAVA
- */
-        System.out.println("hello");;
-        System.out.println("  wassuppppp ! ");
-        System.out.println("NIGAAAAAAAAAA");
-        System.out.println("jaana");
-
-        int age = 45;
-        int year = 2026 ;
-        int quantity = 55 ;
-        System.out.println("The year is " + year);
-        System.out.println("I posses " + quantity + " coke");
-        System.out.println("My age is " + age);
-
-        double price = 29.99;
-        double gpa = 5.5;
-        double temperature = 32.4;
-
-        System.out.println("This is the worst day i have ever seen");
-        System.out.println("I got " + gpa +" cgpa");
-        System.out.println("The gas price is a whopping " + price + " dollars");
-        System.out.println("and the temperature is fucking " +temperature + " celcius");
-
-        char grade = 'b' ;
-        char symbol = '@' ;
-        char currency = '¥';
-
-        System.out.println("I got a " + grade + " grade");
-        System.out.println(symbol);
-        System.out.println(currency);
-
-        boolean ifAvailable =false;
-        boolean isStudent = true;
-        boolean forSale = false;
-        boolean isOnline = true;
-        String name = "Wanda";
-        String food = "PIZZA";
-        String email = "wandavision@gmail.com";
-
-        System.out.println("I am a studen and it is " + isStudent );
-        System.out.println(forSale );
-        System.out.println(isOnline);
-
-        if(isStudent == true) {
-            System.out.println("you are a student !");
-        }
-        else{
-
-            System.out.println("STEP OF BITCH! ");
-        }
-
-        System.out.println("Welcome back young miss, " + name );
-        System.out.println("I heard your kids likeed " + food );
-        System.out.println("you got a few promo on " + email);
-
-        System.out.println("Hello miss "+ name+".");
-        System.out.println("You are currently " + age + " years old in " + year + ".");
-        System.out.println("Today the temperature is " + temperature +" degree celcius");
-        System.out.println("You score a total of " + gpa + " in the year " +year + " and were give " +grade + " grade ");
-        System.out.println("The Pizza is for " + currency +" "+ price);
-        if(ifAvailable == true ){
-            System.out.println("The delivery will be done within 10 minutes");
-        }
-        else{
+//        //THIS MY FIRST java PROGRAM
+//
+//
+///*
+//THIS
+//IS
+//A
+//MULTILINE
+//CODE
+//JAVA
+// */
+//        System.out.println("hello");;
+//        System.out.println("  wassuppppp ! ");
+//        System.out.println("NIGAAAAAAAAAA");
+//        System.out.println("jaana");
+//
+//        int age = 45;
+//        int year = 2026 ;
+//        int quantity = 55 ;
+//        System.out.println("The year is " + year);
+//        System.out.println("I posses " + quantity + " coke");
+//        System.out.println("My age is " + age);
+//
+//        double price = 29.99;
+//        double gpa = 5.5;
+//        double temperature = 32.4;
+//
+//        System.out.println("This is the worst day i have ever seen");
+//        System.out.println("I got " + gpa +" cgpa");
+//        System.out.println("The gas price is a whopping " + price + " dollars");
+//        System.out.println("and the temperature is fucking " +temperature + " celcius");
+//
+//        char grade = 'b' ;
+//        char symbol = '@' ;
+//        char currency = '¥';
+//
+//        System.out.println("I got a " + grade + " grade");
+//        System.out.println(symbol);
+//        System.out.println(currency);
+//
+//        boolean ifAvailable =false;
+//        boolean isStudent = true;
+//        boolean forSale = false;
+//        boolean isOnline = true;
+//        String name = "Wanda";
+//        String food = "PIZZA";
+//        String email = "wandavision@gmail.com";
+//
+//        System.out.println("I am a studen and it is " + isStudent );
+//        System.out.println(forSale );
+//        System.out.println(isOnline);
+//
+//        if(isStudent == true) {
+//            System.out.println("you are a student !");
+//        }
+//        else{
+//
+//            System.out.println("STEP OF BITCH! ");
+//        }
+//
+//        System.out.println("Welcome back young miss, " + name );
+//        System.out.println("I heard your kids likeed " + food );
+//        System.out.println("you got a few promo on " + email);
+//
+//        System.out.println("Hello miss "+ name+".");
+//        System.out.println("You are currently " + age + " years old in " + year + ".");
+//        System.out.println("Today the temperature is " + temperature +" degree celcius");
+//        System.out.println("You score a total of " + gpa + " in the year " +year + " and were give " +grade + " grade ");
+//        System.out.println("The Pizza is for " + currency +" "+ price);
+//        if(ifAvailable == true ){
+//            System.out.println("The delivery will be done within 10 minutes");
+//        }
+//        else{
             System.out.println("Come get it yourself bruv 😭");
         }
 
-    }
+
+        System.out.println("hi");
+
+         }
+
 
 
 }
--- src/main2.java
+++ src/main2.java
@@ -3,55 +3,8 @@ import java.util.Scanner;
 public class main2 {
 
     public static void main(String[] arg){
-
-
-        double width = 0;
-        double height = 0;
-        double area = 0;
-
+        //Mad Libs game
 
         Scanner scanner = new Scanner(System.in);
-
-
-        System.out.print("Enter your Full name : ");
-        String name = scanner.nextLine();
-
-
-        System.out.print("Enter your age ");
-        int age = scanner.nextInt();
-
-        System.out.print("What is your gpa: ");
-        double gpa = scanner.nextDouble();
-
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
-
-
-        System.out.println("Hello " + name);
-        System.out.println("You are " +age + " years old.");
-        System.out.println("You got a cgpa of " +gpa + " in this semester.");
-        if (isStudent == true){
-            System.out.println("You are a student! ");
-        }
-        else {
-            System.out.println("You are NOT a student! ");
-        }
-
-        System.out.print("Width ; ");
-        width = scanner.nextDouble();
-
-        System.out.print("height ; ");
-        height = scanner.nextDouble();
-        scanner.nextLine();
-
-
-        area = width * height;
-       //hii
-
-//        area = scanner.nextDouble();
-        System.out.println("The area of the rectangle is " + area);
-
-        scanner.close();
-
     }
 }
